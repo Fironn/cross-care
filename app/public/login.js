92e7ee8f7c0d8f8ff9e639f55584f5a02c1691ba
Fironn <shihomihashi@gmail.com>
2019-12-15 00:44:20 +0900

    init

--- app/public/login.js
+++ app/public/login.js
@@ -54,7 +54,7 @@ signin.addEventListener('click', function(e) {
     var credential = error.credential;
     // ...
   });
-}
+});
 
 //新規登録処理
 register.addEventListener('click', function(e) {
@@ -77,3 +77,32 @@ login.addEventListener('click', function(e) {
       alert('ログインできません（' + error.message + '）');
     });
   });
+
+  /**
+ * データをPOSTする
+ * @param String アクション
+ * @param Object POSTデータ連想配列
+ * 記述元Webページ http://fujiiyuuki.blogspot.jp/2010/09/formjspost.html
+ * サンプルコード
+ * <a onclick="execPost('/hoge', {'fuga':'fuga_val', 'piyo':'piyo_val'});return false;" href="#">POST送信</a>
+ */
+function execPost(action, data){
+    // フォームの生成
+    var form = document.createElement("form");
+    form.setAttribute("action", action);
+    form.setAttribute("method", "post");
+    form.style.display = "none";
+    document.body.appendChild(form);
+    // パラメタの設定
+    if (data !== undefined) {
+     for (var paramName in data) {
+      var input = document.createElement('input');
+      input.setAttribute('type', 'hidden');
+      input.setAttribute('name', paramName);
+      input.setAttribute('value', data[paramName]);
+      form.appendChild(input);
+     }
+    }
+    // submit
+    form.submit();
+}
--- firestore/functions/index.js
+++ firestore/functions/index.js
@@ -1,8 +1,86 @@
 const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+ 
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+
+exports.setData = functions.https.onRequest((request, response) => {
+  if (request.method === "GET") {
+      admin.database().ref("/users/"+body["userId"]+"/add").once("value")
+          .then(snapshot => {
+                  const products = snapshot.val();
+                  const array = Object.keys(products).map(key => products[key]);
+
+                  console.log("reference success", array);
+                  response.status(200).json(array).end();
+              }
+          ).catch(error => {
+              console.log("reference error", error);
+              response.status(500).end();
+          }
+      );
+
+  } else if (request.method === "POST") {
+      var now = new Date();
+      const body = request.body;
+      const pushRef = admin.database().ref("/users/"+body["userId"]+"/add");
+
+      pushRef.set({
+          add: body["add"],
+          addType: body["type"],
+          update: now
+      }, error => {
+
+          if (error) {
+              console.log("save error", error.message);
+              response.status(500).send(error.message).end();
+          } else {
+              console.log("save success");
+              response.status(200).end();
+          }
+      });
+
+  } else response.status(404).end();
+});
+
+exports.usersDetail = functions.https.onRequest((request, response) => {
+  if (request.method === "GET") {
+      admin.database().ref("/users/"+body["userId"]+"/detail").once("value")
+          .then(snapshot => {
+                  const products = snapshot.val();
+                  const array = Object.keys(products).map(key => products[key]);
+
+                  console.log("reference success", array);
+                  response.status(200).json(array).end();
+              }
+          ).catch(error => {
+              console.log("reference error", error);
+              response.status(500).end();
+          }
+      );
+
+  } else if (request.method === "POST") {
+      var now = new Date();
+      const body = request.body;
+      const pushRef = admin.database().ref("/users/"+body["userId"]+"/detail");
+
+      pushRef.set({
+          userName: body["userName"],
+          eggName: body["eggName"]
+      }, error => {
+
+          if (error) {
+              console.log("save error", error.message);
+              response.status(500).send(error.message).end();
+          } else {
+              console.log("save success");
+              response.status(200).end();
+          }
+      });
+
+  } else response.status(404).end();
+});
+
