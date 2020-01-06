4d3dbf29d4db1e5aa541978c5a6d647f31f84da7
Fironn <shihomihashi@gmail.com>
2020-01-06 09:43:37 +0900

    2 window

---
+++ app/public/egg.js
@@ -0,0 +1,53 @@
+var x=1;
+var point=10;
+var lev=1;
+const vw=screen.width;
+const vh=screen.height;
+const userId='1';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyChJyeair2I4TkizlQD91yY2JEJ8CdFhq8",
+    authDomain: "cross-care-d73be.firebaseapp.com",
+    projectId: "cross-care-d73be",
+    databaseURL: "https://cross-care-d73be.firebaseio.com"
+});
+var functions = firebase.functions();
+
+var addMessage = firebase.functions().httpsCallable('getDetail');
+addMessage({userId:userId}).then(function(result) {
+  console.log(result.data);
+  document.getElementById("point").innerText=result.data[1];
+  document.getElementById("userName").innerText=result.data[0];
+  document.getElementById("petName").innerText=result.data[3];
+  point=result.data[1];
+  x=point/100;
+  lev=Math.floor(point/100);
+  document.getElementById("level").innerText=lev;
+  window.onload();
+}).catch( error =>  console.log(error) );;
+
+window.onload=function(){
+    // var levelBar = document.getElementById('levelBar');
+    // var levBcontext = levelBar.getContext('2d');
+    // levBcontext.fillRect(0,0,point,20);
+    // levBcontext.fillStyle = 'rgb(255,00,00)';
+    // levBcontext.fill();
+    
+    var cvs=document.getElementById("egg");
+    var ctx=cvs.getContext("2d");
+    ctx.fillStyle="black";
+    ctx.beginPath();
+    ctx.arc(vw/2,vh/2,40*x,0,Math.PI*2);
+    ctx.fill();
+}
+
+large=function(){
+    x+=0.03;
+    point+=10;
+    document.getElementById("point").innerText=point;
+    if(point>lev*100&&point%(lev*100)==0){
+        lev++;
+        document.getElementById("level").innerText=lev;
+    }
+    window.onload();
+}
--- firestore/functions/index.js
+++ firestore/functions/index.js
@@ -1,87 +1,89 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+
 admin.initializeApp(functions.config().firebase);
  
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+exports.helloWorld = functions.https.onCall((data, context) => {
+    return{text:"Hello from Firebase!"};
 });
 
-exports.setData = functions.https.onRequest((request, response) => {
-  if (request.method === "GET") {
-      const param = request.query;
-      admin.database().ref("/users/"+param["userId"]+"/add").once("value")
-          .then(snapshot => {
-                  const products = snapshot.val();
-                  const array = Object.keys(products).map(key => products[key]);
-
-                  console.log("reference success", array);
-                  response.status(200).json(array).end();
-              }
-          ).catch(error => {
-              console.log("reference error", error);
-              response.status(500).end();
-          }
-      );
-
-  } else if (request.method === "POST") {
-      const body = request.body;
-      const pushRef = admin.database().ref("/users/"+body["userId"]+"/add");
-
-      pushRef.set({
-          add: body["add"],
-          addType: body["type"],
-          update: body["update"]
-      }, error => {
-
-          if (error) {
-              console.log("save error", error.message);
-              response.status(500).send(error.message).end();
-          } else {
-              console.log("save success");
-              response.status(200).end();
-          }
-      });
-
-  } else response.status(404).end();
+exports.getData = functions.https.onCall((data,context) => {
+    const userId = data.userId;
+    if (!userId) {
+        throw new functions.https.HttpsError('put the user id');
+    }
+    return admin.database().ref("/users/"+userId+"/add").once("value")
+    .then(snapshot => {
+            const products = snapshot.val();
+            const array = Object.keys(products).map(key => products[key]);
+            return array;
+        }
+    ).catch(error => {
+        throw new functions.https.HttpsError('unknown', error.message, error);
+    });
 });
 
-exports.usersDetail = functions.https.onRequest((request, response) => {
-  if (request.method === "GET") {
-      const param = request.query;
-      admin.database().ref("/users/"+param["userId"]+"/detail").once("value")
-          .then(snapshot => {
-                  const products = snapshot.val();
-                  const array = Object.keys(products).map(key => products[key]);
-
-                  console.log("reference success", array);
-                  response.status(200).json(array).end();
-              }
-          ).catch(error => {
-              console.log("reference error", error);
-              response.status(500).end();
-          }
-      );
-
-  } else if (request.method === "POST") {
-      var now = new Date();
-      const body = request.body;
-      const pushRef = admin.database().ref("/users/"+body["userId"]+"/detail");
-
-      pushRef.set({
-          userName: body["userName"],
-          eggName: body["eggName"],
-          update: body["update"]
-      }, error => {
-
-          if (error) {
-              console.log("save error", error.message);
-              response.status(500).send(error.message).end();
-          } else {
-              console.log("save success");
-              response.status(200).end();
-          }
-      });
-
-  } else response.status(404).end();
+exports.setData = functions.https.onCall((data, context) => {
+    const userId = data.userId;
+    const add=data.add;
+    const type=data.type;
+    const update=data.text.update;
+    if(!userId||!add||!type||!update){
+        throw new functions.https.HttpsError('put the data');
+    }
+    const pushRef = admin.database().ref("/users/"+userId+"/add");
+    pushRef.set({
+        add: add,
+        addType: type,
+        update: update
+    }, error => {
+        if (error) {
+            console.log("save error", error.message);
+            throw new functions.https.HttpsError('unknown', error.message, error);
+        } else {
+            console.log("save success");
+            throw new functions.https.HttpsError('unknown', error.message, error);
+        }
+    });
+    return data;
 });
 
+exports.getDetail = functions.https.onCall((data, context) => {
+    const userId = data.userId;
+    if (!userId) {
+        throw new functions.https.HttpsError('put the user id');
+    }
+    return admin.database().ref("/users/"+userId+"/detail").once("value")
+    .then(snapshot => {
+            const products = snapshot.val();
+            const array = Object.keys(products).map(key => products[key]);
+            return array;
+        }
+    ).catch(error => {
+        throw new functions.https.HttpsError('unknown', error.message, error);
+    });
+});
+
+exports.setDetail = functions.https.onCall((data, context) => {
+    const userName=data.userName;
+    const eggName=data.eggName;
+    const update=data.text.update;
+    if(!userName||!eggName||!update){
+        throw new functions.https.HttpsError('put the data');
+    }
+    const pushRef = admin.database().ref("/users/"+userId+"/add");
+    pushRef.set({
+        add: add,
+        addType: type,
+        update: update
+    }, error => {
+        if (error) {
+            console.log("save error", error.message);
+            throw new functions.https.HttpsError('unknown', error.message, error);
+        } else {
+            console.log("save success");
+            throw new functions.https.HttpsError('unknown', error.message, error);
+        }
+    });
+    return data;
+});
